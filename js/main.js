e842a0550692df37bb410a99f4f3de35ff38f6cd
mañana <alvaro.gracia.reg@gmail.com>
2023-04-05 13:38:34 +0200

    reestart

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 /*menu*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-     
+const navMenu = document.getElementById(''),
+      navToggle = document.getElementById(''),
+      navClose = document.getElementById('')
 
-if(navToggle) {
-    navToggle.addEventListener('click', () =>  {
-        navMenu.classList.add('show-menu')
-    })
-}
+      if(navToggle) {
+        navToggle.addEventListener('click', () =>  {
+            navMenu.classList.add('show-menu')
+        })
+    }
+    
+    if(navClose) {
+        navClose.addEventListener('click', () =>  {
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
-if(navClose) {
-    navClose.addEventListener('click', () =>  {
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-/* remove menu mobile*/
+    /* remove menu mobile*/
 const navLink = document.querySelectorAll('.navLink')
 
 function linkAction() {
@@ -26,72 +25,6 @@ function linkAction() {
 }
 
 navLink.forEach(n =>  n.addEventListener('click', linkAction))
-
-/*swiper nuevos slider */
-
-let newSwiper = new Swiper (".new-swiper", {
-    centeredSlides: true,
-    slidesPerView: "auto",
-    loop: 'true',
-    spaceBetween: 16,
-})
-
-
-
-/*Background header*/
-
-function scrollHeader() {
-    const header = document.getElementById('header')
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
-    else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader)
-
-/*scroll section*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-    const scrollY = window.pageYOffset 
-    sections.forEach(current =>  {
-        const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop -58,
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.navM a[href*='+ sectionId + ']').classList.add('active-link')
-        }
-        else {
-            document.querySelector('.navM a[href*='+ sectionId + ']').classList.remove('active-link')
-        }
-    } )
-}
-window.addEventListener('scroll', scrollActive)
-
-
-/*scroll up*/
-
-
-function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 460 ) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove('show-scroll')
-}
-
-window.addEventListener('scroll', scrollUp)
-
-
-/*Animation*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2000,
-    delay: 200
-})
-
-sr.reveal(`.home-swiper, .new-swiper, .newslc`)
-sr.reveal(`.categorydata, .footercontent`, {interval: 100})
-sr.reveal(`.aboutdata, .discountimg`, {origin: 'left'})
-sr.reveal(`.aboutimg, .discountdata`, {origin: 'left'})
 
 //THEMES
 
@@ -123,14 +56,3 @@ function toggleTheme() {
        setTheme('theme-light');
    }
 })();
-
-//OPEN DIVS OF WEB PROJECTS
-
-const btns_opendescription = document.querySelectorAll(".btnwebproject")
-
-btns_opendescription.forEach(button=>{
-    button.addEventListener("click", ()=>{
-
-        
-    })
-})
