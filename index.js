c2374196c9f96950d7635e84795dba0c8dc25367
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-05-22 09:26:46 +0200

    http

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
 var fs = require("fs");
 var path = require("path");
+
 var server = http.createServer(function (req, res) {
     // Obtener la ruta del archivo index.html
     var indexPath = path.join(__dirname, 'public', 'index.html');
     // Si la petición es para la ruta principal, enviar el archivo index.html
     if (req.url === '/') {
-        fs.readFile(indexPath, function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                res.end("Error interno del servidor: ".concat(err));
-            }
-            else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    }
-    else {
-        // Si no es la ruta principal, devolver un error 404
-        res.writeHead(404);
-        res.end('Página no encontrada');
+        sendfile(indexPath, res)
+
+    }else {
+       indexPath = path.join(__dirname, 'public', decodeURI(req.url))
+       sendfile(indexPath, res)
     }
 });
 // Iniciar el servidor
@@ -30,3 +21,17 @@ var PORT = process.env.PORT || 3000;
 server.listen(PORT, function () {
     console.log("Servidor iniciado en el puerto ".concat(PORT));
 });
+
+function sendfile(path, res){
+
+    fs.readFile(path, function (err, data) {
+
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'})
+            res.end('Página no encontrada')
+        }else {
+            res.writeHead(200);
+            res.end(data);
+        }
+    });
+}
